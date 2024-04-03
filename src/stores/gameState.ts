fcdaf961fed717343068f5dc4acb3a0eeeb6db39
Sacramenitx <benji.u1225u@gmail.com>
2024-04-03 14:01:17 +0200

    completed initial gameState example

--- src/stores/gameState.ts
+++ src/stores/gameState.ts
@@ -27,10 +27,23 @@ export const gameState = reactive({
     rounds: [{
         id: 0,
         answer: 5, // The value of the correct answer added at the end of the round
-        
-    }]
+        review: "Le kebab est très bon surtout avec la sauce du chef!",
+        startTime: 1712143227058,
+        guesses: [{
+            from: 0,
+            value: 5
+        }, {
+            from: 1,
+            value: 4
+        }]
+    }],
+    settings: {
+        numberOfRound: 5,
+        timePerRound: 20,
+        maxPlayers: 15
+    }
 })
 
 export function useGameState() {
-
+    return gameState;
 }

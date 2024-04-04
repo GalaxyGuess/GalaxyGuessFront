430e41bc7c6028dbb014df222b7f75c57da85a27
Arthur Madec-Prévost <arthur.madecprevost@protonmail.com>
2024-04-04 13:54:50 +0200

    rajout une étoile

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -12,7 +12,7 @@ const goToJoinPage = () => {
 
 <template>
     <main class="Home-page">
-        <h1>⭐️ Le jeu des étoiles</h1>
+        <h1>⭐️ Le jeu des étoiles ⭐️</h1>
         <p>
             Trouve la notation des avis avant tes amis!
         </p>

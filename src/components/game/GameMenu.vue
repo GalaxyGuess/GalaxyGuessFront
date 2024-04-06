3b329fcd55266ac47e731bb857bb874760a10c65
Sacramenitx <benji.u1225u@gmail.com>
2024-04-06 19:22:39 +0200

    ✨ add GameMenu and GamePlayground component

---
+++ src/components/game/GameMenu.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { useGameState } from "../../stores/gameState";
+
+const { gameState } = useGameState();
+
+</script>
+<template>
+    <section class="Game-menu">
+        <ul>
+            <li v-for="player in gameState.players">
+                {{ player.username }} score: {{ player.score }}
+            </li>
+        </ul>
+    </section>
+</template>
+<style scoped lang="scss">
+.Game-menu {
+    display: grid;
+    place-items: center;
+    background: #0f01;
+}
+</style>
---
+++ src/components/game/GamePlayground.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+// import { useGameState } from "../../stores/gameState";
+
+// const { gameState } = useGameState();
+
+</script>
+<template>
+    <section class="Game-playground">
+        playground
+    </section>
+</template>
+<style scoped lang="scss">
+.Game-playground {
+    display: grid;
+    place-items: center;
+    background: #0f01;
+}
+</style>
